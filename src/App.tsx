2d8660887c89a8ff3d1055ac2eca907617dc5d05
vburmus <burmusvolod@gmail.com>
2023-09-01 00:34:43 +0200

    feat(react_task) initialized react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from "react";
+
+
+function App() {
+    return (
+       <></>
+    );
+}
+
+export default App;
