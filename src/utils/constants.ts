65eed98ef8452883960533927c6a5e7b164444a8
vburmus <burmusvolod@gmail.com>
2023-09-01 00:48:56 +0200

    fix(react_task) added constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,28 @@
+export const DATE_ASC = "date";
+export const DATE_DESC = "-date";
+export const DESC = "desc";
+export const ASC = "asc";
+export const NAME_ASC = "name";
+export const NAME_DESC = "-name";
+export const PAGE = 'page';
+export const SIZE = 'size';
+export const SORT = 'sort';
+export const WRONG_SORT_DIRECTION = "Wrong sort direction";
+export const WRONG_SORT_PARAMS = "Wrong sort params";
+export const WRONG_SORT_PARAM = "Wrong sort parameter";
+export const WRONG_NUMBER_OF_SORT_PARAMS = "Wrong number of sort elements";
+export const CHANGE_THE_CRITERIA = "Change the criteria";
+export const CHECK_THE_FORM = "Check the form";
+export const NO_SERVER_RESPONSE = 'No Server Response';
+export const UPDATE_FAILED = 'Update failed';
+export const ADMIN = "ADMIN";
+export const TAG_REGEX = /#\(([^)]+)\)/g
+
+export const OBJECT_NAME_REGEX = /^[A-Z0-9\s]{3,15}$/i;
+export const SHORT_REGEX = /^.{10,50}$/;
+export const LONG_REGEX = /^.{100,600}$/;
+export const NAME_SURNAME_REGEX = /^[A-Z]{3,23}$/i;
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_]).{8,24}$/;
+export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Z]{2,}$/i;
+export const NUMBER_REGEX = /^[0-9\-\+]{9,15}$/;
+export const ALLOWED_IMG = ["image/jpg", "image/jpeg", "image/png", "image/gif"];
