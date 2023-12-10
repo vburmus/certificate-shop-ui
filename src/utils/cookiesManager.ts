f09b157e674288ae3b6ff2339c5d06e22adfdbe9
vburmus <burmusvolod@gmail.com>
2023-12-10 11:17:07 +0100

    feat(microservices-ui) added dateUtils and updated cookies manager

--- src/utils/cookiesManager.ts
+++ src/utils/cookiesManager.ts
@@ -2,11 +2,11 @@ import Cookies from 'universal-cookie';
 
 const cookies: Cookies = new Cookies();
 
-export const setCookie = (name: string, value: string) => {
-    cookies.set(name, value);
+export const setCookie = (name: string, value: string, options = {}) => {
+    cookies.set(name, value, options);
 }
 
-export const getCookie = (name: string): string | undefined => {
+export const getCookie = (name: string) => {
     return cookies.get(name);
 }
 
---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,10 @@
+export const formatDate = (dateStr: string) => {
+    const date = new Date(dateStr)
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${hours}:${minutes} ${day}.${month}.${year}`;
+}
