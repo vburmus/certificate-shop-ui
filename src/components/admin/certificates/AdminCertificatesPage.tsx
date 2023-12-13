4df3f680307a13823c35dbee1b18a599718a119a
vburmus <burmusvolod@gmail.com>
2023-12-13 20:21:39 +0100

    fix(microservices-ui) modified admin tags page

--- src/components/admin/certificates/AdminCertificatesPage.tsx
+++ src/components/admin/certificates/AdminCertificatesPage.tsx
@@ -1,16 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import PaginationList from "./PaginationList";
-import {useLocation} from "react-router-dom";
-import {Certificate, Sort} from "../../utils/types";
+import PaginationList from "../../common/PaginationList";
+import {useLocation, useNavigate} from "react-router-dom";
+import {Certificate, Sort} from "../../../utils/types";
 import CertificatesTable from "./CertificatesTable";
-import {getCertificates} from "../../utils/certificateUtils";
+import {getCertificates} from "../../../utils/certificateUtils";
 import {isAxiosError} from "axios";
-import Loader from "../common/Loader";
+import Loader from "../../common/Loader";
 import {useSelector} from "react-redux";
-import RootState from "../../redux/RootState";
+import RootState from "../../../redux/RootState";
 import {
     ASC,
     CHANGE_THE_CRITERIA,
+    DATE,
     DATE_ASC,
     DATE_DESC,
     DESC,
@@ -23,11 +24,12 @@ import {
     WRONG_SORT_DIRECTION,
     WRONG_SORT_PARAM,
     WRONG_SORT_PARAMS
-} from '../../utils/constants';
+} from '../../../utils/constants';
+import {Button} from "reactstrap";
 
 
-const AdminUI = () => {
-    const [error, setError] = useState('')
+const AdminCertificatesPage = () => {
+    const [error, setError] = useState("")
     const [certificates, setCertificates] = useState<Certificate[]>([])
     const [isLoading, setIsLoading] = useState(true)
     const [totalPages, setTotalPages] = useState(0)
@@ -37,8 +39,10 @@ const AdminUI = () => {
     let currentPage = page ? parseInt(page) : 1
     const size = queryParams.get(SIZE);
     const currentSize = size ? parseInt(size) : 5
+    const [totalElems, setTotalElems] = useState(0)
     const sortParams = queryParams.getAll(SORT)
     const filterState = useSelector((state: RootState) => state.filter)
+    const navigate = useNavigate();
     const parseSort = () => {
         const sort: Sort = {
             sortDate: "",
@@ -47,7 +51,7 @@ const AdminUI = () => {
         if (sortParams.length === 2) {
             const date = sortParams[0].split(":")
             const name = sortParams[1].split(":")
-            if (date[0] === DATE_ASC) {
+            if (date[0] === DATE) {
                 if (date[1] === DESC)
                     sort.sortDate = DATE_DESC
                 else if (date[1] === ASC)
@@ -68,7 +72,7 @@ const AdminUI = () => {
             const param = sortParams[0].split(":")
             if (param[0] === NAME_ASC) {
                 sort.sortName = param[1] === ASC ? NAME_ASC : NAME_DESC
-            } else if (param[0] === DATE_ASC) {
+            } else if (param[0] === DATE) {
                 sort.sortName = param[1] === ASC ? DATE_ASC : DATE_DESC
             } else {
                 setError(WRONG_SORT_PARAM)
@@ -85,7 +89,8 @@ const AdminUI = () => {
             setIsLoading(true)
             const data = await getCertificates(filterState, currentPage, currentSize, parseSort())
             setTotalPages(data.totalPages)
-            setCertificates(data.content)
+            data.content.length > 0 ? setCertificates(data.content) : setError(CHANGE_THE_CRITERIA)
+            setTotalElems(data.totalElements)
         } catch (e) {
             if (isAxiosError(e) && e.response)
                 if (e.response.status === 404)
@@ -107,13 +112,17 @@ const AdminUI = () => {
     }, [filterState, location]);
 
     return (isLoading ? <Loader/> :
-            <div className="d-flex flex-row flex-wrap justify-content-center lign-self-start mt-2"
-                 style={{width: "80%"}}>
-                {error && <h3 className="alert alert-danger">{error}</h3>}
-                <CertificatesTable certificates={certificates}/>
-                <PaginationList totalPages={totalPages} currentPage={currentPage} currentSize={currentSize}/>
-            </div>
+            error ? <div className="alert alert-info d-flex flex-column align-items-center w-25">
+                    <h3>{error}</h3>
+                    <Button className="btn btn-info w-50" onClick={() => navigate("/")}>Clear search</Button>
+                </div> :
+                <div className="d-flex flex-row flex-wrap justify-content-center lign-self-start mt-2"
+                     style={{width: "80%"}}>
+                    <CertificatesTable certificates={certificates}/>
+                    <PaginationList totalPages={totalPages} currentPage={currentPage} currentSize={currentSize}
+                                    totalElems={totalElems}/>
+                </div>
     );
 };
 
-export default AdminUI;
+export default AdminCertificatesPage;
