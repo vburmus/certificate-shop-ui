45cbdf1f9bd80b5736116a9cbe448ddbb819b5b8
vburmus <burmusvolod@gmail.com>
2023-12-14 13:23:59 +0100

    fix(microservices-ui) modified home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,48 +1,42 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import RootState from "../redux/RootState";
-import InfiniteScroll from "react-infinite-scroll-component";
 import axiosInstance from "../utils/axiosConfig";
 import Loader from "./common/Loader";
 import 'styles/css/Home.css'
-import CertificateList from "./CertificateList";
-import TagList from "./TagList";
+import CertificateList from "./certificates/CertificateList";
+import TagList from "./tags/TagList";
 import {CaretLeft, CaretRight} from "react-bootstrap-icons";
-import _ from "lodash";
-import {setCurrentPage, setLoadingPage} from "../redux/pageSlice";
 import {isAxiosError} from "axios";
 import {getCertificates} from "../utils/certificateUtils";
-import {CHANGE_THE_CRITERIA, NO_SERVER_RESPONSE} from "../utils/constants";
+import {CHANGE_THE_CRITERIA, NO_SERVER_RESPONSE, PAGE, SIZE} from "../utils/constants";
+import PaginationList from "./common/PaginationList";
+import {useLocation} from "react-router-dom";
+import {Button} from "reactstrap";
+import {clearFilters} from "../redux/filterSlice";
 
 const Home = () => {
     const [tags, setTags] = useState<any[]>([])
     const [tagsPage, setTagsPage] = useState(1)
     const [isLoadingTags, setIsLoadingTags] = useState<boolean>(true);
     const [hasMoreTags, setHasMoreTags] = useState(true);
-
     const [certificates, setCertificates] = useState<any[]>([]);
     const [isLoadingCertificates, setIsLoadingCertificates] = useState<boolean>(true);
-    const certificatePage = useSelector((state: RootState) => state.page)
-    const [hasMoreCertificates, setHasMoreCertificates] = useState(true)
-
+    const [totalPages, setTotalPages] = useState(0)
+    const [totalElems, setTotalElems] = useState(0)
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search);
+    const page = queryParams.get(PAGE);
+    let currentPage = page ? parseInt(page) : 1
+    const size = queryParams.get(SIZE);
+    const currentSize = size ? parseInt(size) : Math.floor((window.innerWidth * 0.8) / 300) * 2
     const filterState = useSelector((state: RootState) => state.filter)
-    const size = Math.floor((window.innerWidth * 0.8) / 300) * 2
     const [error, setError] = useState('');
     const dispatch = useDispatch()
-
-    const handlePageChange = _.debounce((page: number) => {
-        dispatch(setCurrentPage(page));
-    }, 300, {trailing: true});
-
-    const handleLoadChange = _.debounce((loading: boolean) => {
-        dispatch(setLoadingPage(loading))
-        setIsLoadingCertificates(loading)
-    }, 300, {trailing: true});
-
     const getTags = async () => {
         try {
             setIsLoadingTags(true);
-            const response = await axiosInstance.get(`/tag?page=${tagsPage}&size=${size/2}`)
+            const response = await axiosInstance.get(`api/v1/commerce/tags?page=${tagsPage - 1}&size=${Math.floor((window.innerWidth * 0.8) / 300)}`)
             const data = response.data
             setTags(data.content)
             setHasMoreTags(!data.last)
@@ -59,66 +53,67 @@ const Home = () => {
         }
     }
 
-    const getMoreCertificates = async () => {
-        if (filterState.loading)
-            return;
+    const getCertificatesPage = async () => {
         try {
-            handleLoadChange(true)
             setError("")
-            if (certificatePage.currentPage === 1) {
-                setCertificates([]);
-                setHasMoreCertificates(true);
-            }
-            const data = await getCertificates(filterState,certificatePage.currentPage,size )
-            setCertificates(prevItems => [...prevItems, ...data.content])
-            handlePageChange(certificatePage.currentPage + 1)
-            setHasMoreCertificates(!data.last)
+            setIsLoadingCertificates(true)
+
+            let data = await getCertificates(filterState, currentPage, currentSize)
+            setTotalPages(data.totalPages)
+            setTotalElems(data.totalElements)
+            data.content.length > 0 ? setCertificates(data.content) : setError(CHANGE_THE_CRITERIA)
+
         } catch (e) {
             if (isAxiosError(e) && e.response)
-                if (e.response.status === 404)
-                    setError(CHANGE_THE_CRITERIA)
-                else
-                    setError(e.response.data.detail)
+                setError(e.response.data.detail)
         } finally {
-            handleLoadChange(false)
+            setIsLoadingCertificates(false)
         }
     }
 
     useEffect(() => {
+        getCertificatesPage()
+    }, [currentPage]);
+
+    useEffect(() => {
         setCertificates([]);
-        getMoreCertificates();
-    }, [filterState]);
+        getCertificatesPage()
+    }, [filterState, location]);
 
     useEffect(() => {
         getTags();
     }, [tagsPage]);
 
-    useEffect(() => {
-        return () => {
-            handlePageChange(1);
-        };
-    }, []);
 
-    return (isLoadingTags && isLoadingCertificates ? <Loader/> :
-            <>
-                <div className="tags">
-                    {!isLoadingTags && tagsPage !== 1 &&
-                        <CaretLeft className="arrow arrow-left" onClick={() => setTagsPage(tagsPage - 1)} size={24}/>
+    return (error ?
+            <div className="align-self-center alert alert-info  align-self-start ">
+                <h5>{error}</h5>
+                <Button className="w-100 btn-info" onClick={() => dispatch(clearFilters())}>Go Back</Button>
+            </div>
+            : isLoadingTags && isLoadingCertificates ?
+                <Loader/> :
+                <>
+                    <div className="tags shadow">
+                        {!isLoadingTags && tagsPage !== 1 &&
+                            <CaretLeft className="arrow arrow-left" onClick={() => setTagsPage(tagsPage - 1)}
+                                       size={24}/>
+                        }
+                        {isLoadingTags ? <Loader/> : <TagList tags={tags}/>}
+                        {!isLoadingTags && hasMoreTags &&
+                            <CaretRight className="arrow-right" onClick={() => setTagsPage(tagsPage + 1)} size={24}/>
+                        }
+                    </div>
+                    {isLoadingCertificates ? <Loader/> :
+
+                        <>
+                            <div className="certificates">
+                                <CertificateList certificates={certificates}/>
+                            </div>
+                            <PaginationList totalPages={totalPages} currentPage={currentPage} currentSize={currentSize}
+                                            totalElems={totalElems}/>
+                        </>
                     }
-                    {isLoadingTags ? <Loader/> : <TagList tags={tags}/>}
-                    {!isLoadingTags && hasMoreTags &&
-                        <CaretRight className="arrow arrow-right" onClick={() => setTagsPage(tagsPage + 1)} size={24}/>
-                    }
-                </div>
-                {!error ?
-                    <InfiniteScroll next={getMoreCertificates} className="certificates" hasMore={hasMoreCertificates}
-                                    loader={<Loader/>}
-                                    dataLength={certificates.length}>
-                        <CertificateList certificates={certificates}/>
-                    </InfiniteScroll>
-                    :
-                    <h5 className="alert alert-warning">{error}</h5>}
-            </>
+                </>
     );
 };
 export default Home;
